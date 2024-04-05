29924fbc3654eab39277c1010a3a7edfe1762b43
Erik Vera <apcxiii@gmail.com>
2024-04-04 18:28:43 -0600

    update schema and changeset to support new fields and validations

--- lib/mini_viking/accounts/user.ex
+++ lib/mini_viking/accounts/user.ex
@@ -6,19 +6,26 @@ defmodule MiniViking.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @attrs [:first_name, :last_name, :email, :role, :job_title, :about, :status]
+  @attrs_required [:first_name, :last_name, :email, :role, :status]
+
   schema "users" do
     field :role, :string
     field :first_name, :string
     field :last_name, :string
     field :email, :string
-
+    field :job_title, :string
+    field :about, :string
+    field :status, :boolean
     timestamps(type: :utc_datetime)
   end
 
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:first_name, :last_name, :email, :role])
-    |> validate_required([:first_name, :last_name, :email, :role])
+    |> cast(attrs, @attrs)
+    |> validate_required(@attrs_required)
+    |> validate_length(:job_title, max: 150)
+    |> validate_length(:about, max: 3000)
   end
 end
